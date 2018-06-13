963fec019fceb8231cf688a492fca2263be1cea0
liting <liting@matchvs.com>
2018-06-13 19:42:16 +0800

    delte

--- 使用指南/快速入门/1.sh
+++
@@ -1,9 +0,0 @@
- #============ get the file name ===========    
-Folder_A="H:\git\Doc\Doc\使用指南\快速入门"    
-for file_a in ${Folder_A}/*  
-do    
-    temp_file=`basename $file_a` 
-	str=$"$temp_file \n"
-	sstr=$(echo -e $str)
-    echo $str >> 2.txt   
-done    
